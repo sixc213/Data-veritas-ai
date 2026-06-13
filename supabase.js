01b80184f6273276a9008ecc1683417ea1ea2c51
sixc213 <ew846346@gmail.com>
2026-06-13 19:36:06 +0100

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "gqqtpsmnafoiaaklxkzt";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxcXRwc21uYWZvaWFha2x4a3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjQ2MTUsImV4cCI6MjA5Njk0MDYxNX0.eXjYJEdz6ndJZPFhTbC9kurVRUX6iBC8k6J_qUSYmHM";
+
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
